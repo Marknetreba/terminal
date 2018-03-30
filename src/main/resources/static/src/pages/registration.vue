d62025d25c4bb82ad2830cc17c226bac9473befc
mark <mark.netreba@mail.ru>
2018-03-30 18:45:37 +0400

    fix

--- src/main/resources/static/src/pages/registration.vue
+++ src/main/resources/static/src/pages/registration.vue
@@ -73,7 +73,7 @@
     data() {
       return {
         tabIndex: 0,
-        macAddress: '55',
+        macAddress: '',
         label: "Пожалуйста, подождите...",
         progress: false,
         response: [],
@@ -115,7 +115,7 @@
     name: "Registration",
     created() {
       this.id = window.location.href;
-      //this.macAddress = config[this.id.substring(this.id.indexOf('=')+1,this.id.indexOf('#'))];
+      this.macAddress = config[this.id.substring(this.id.indexOf('=')+1,this.id.indexOf('#'))];
       console.log(window.location)
     },
     methods: {
