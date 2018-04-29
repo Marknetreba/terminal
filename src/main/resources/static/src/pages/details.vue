07cea5e9eaa03088651498c1728bad3635e34e65
Marknetreba <mark.netreba@mail.ru>
2018-04-30 02:40:57 +0400

    pochti vse

--- src/main/resources/static/src/pages/details.vue
+++ src/main/resources/static/src/pages/details.vue
@@ -2,14 +2,16 @@
   <div class="main_details">
     <b_img class="logo" src="../photo/55/start.png"></b_img>
     <loading :show="progress" :label="label"></loading>
-    <p style="position: relative; right: 335px; color: #009800; font-size: 30px" class="mt-3 mb-4"><strong>{{msg}}</strong></p>
+    <p style="position: relative; right: 335px; color: #009800; font-size: 30px" class="mt-3 mb-4">
+      <strong>{{msg}}</strong></p>
     <div class="options">
 
       <div style="background: #ffd310; width: 49%">
 
         <a><strong>{{items[0].fullname}}, Вам сегодня предстоит прием у специалистов</strong></a><br/><a><strong>{{items[0].webname}}!</strong></a><br/>
 
-        <b_table class="mt-3" striped hover :items="items" :fields="fields" style="background-color: white" @row-clicked="itemClick">
+        <b_table class="mt-3" striped hover :items="items" :fields="fields" style="background-color: white"
+                 @row-clicked="itemClick">
           <template slot="time" slot-scope="data">
             {{data.item.bhour}}:{{data.item.bmin}}
           </template>
@@ -24,9 +26,13 @@
         <alert variant="success" :show="check">Благодарим вас за регистрацию посещения</alert>
         <button class="btn btn-lg mt-3" @click="goBack">Назад</button>
         <button v-bind:disabled="disabled" class="btn btn-lg mt-3" @click="checkIncome">Я пришел(ла)</button>
+        <button class="btn btn-warning btn-lg mt-3" @click="notify">Уведомление</button>
+
       </div>
 
-      <div class="details_iframe"><b_img src="../photo/55/sales/55.png" style="width: 100%; height: 100%"/></div>
+      <div class="details_iframe">
+        <b_img src="../photo/55/sales/55.png" style="width: 100%; height: 100%"/>
+      </div>
     </div>
 
   </div>
@@ -34,95 +40,126 @@
 </template>
 
 <script>
-    import Card from "bootstrap-vue/es/components/card/card";
-    import b_checkbox from "bootstrap-vue/es/components/form-checkbox/form-checkbox"
-    import Button from "bootstrap-vue/es/components/button/button";
-    import router from '../router/index';
-    import b_table from "bootstrap-vue/src/components/table/_table";
-    import modal from "bootstrap-vue/es/components/modal/_modal";
-    import b_img from "bootstrap-vue/es/components/image/img";
-    import loading from 'vue-full-loading';
-    import moment from 'moment';
-    import alert from "bootstrap-vue/es/components/alert/alert";
-    import config from "../config.json";
+  import Card from "bootstrap-vue/es/components/card/card";
+  import b_checkbox from "bootstrap-vue/es/components/form-checkbox/form-checkbox"
+  import Button from "bootstrap-vue/es/components/button/button";
+  import router from '../router/index';
+  import b_table from "bootstrap-vue/src/components/table/_table";
+  import modal from "bootstrap-vue/es/components/modal/_modal";
+  import b_img from "bootstrap-vue/es/components/image/img";
+  import loading from 'vue-full-loading';
+  import moment from 'moment';
+  import alert from "bootstrap-vue/es/components/alert/alert";
+  import config from "../config.json";
 
-    export default {
-      components: {modal, Button, Card, b_table, b_checkbox, b_img, loading, alert, config},
-      name: "Details",
-      data() {
-        return {
-          items: [],
-          disabled: false,
-          fields:{
-            time:{label: 'Время приема', sortable: true},
-            chname:{label: 'Кабинет врача', sortable: true},
-            docname:{label: 'ФИО врача', sortable: true},
-            clvisit:{label: 'Пришел(ла)', sortable: true}
+  export default {
+    components: {modal, Button, Card, b_table, b_checkbox, b_img, loading, alert, config},
+    name: "Details",
+    data() {
+      return {
+        items: [],
+        disabled: false,
+        fields: {
+          time: {label: 'Время приема', sortable: true},
+          chname: {label: 'Кабинет врача', sortable: true},
+          docname: {label: 'ФИО врача', sortable: true},
+          clvisit: {label: 'Пришел(ла)', sortable: true}
+        },
+        table: [],
+        show: false,
+        label: "Пожалуйста, подождите...",
+        progress: false,
+        msg: "ДЕТАЛИ ПРИЕМА",
+        check: false,
+        macAddress: '',
+        imagePath: location.origin+'/images/'
+      }
+    },
+    beforeRouteEnter: (to, from, next) => {
+      next(vm => {
+          vm.items = vm.$store.getters['registration/getData'];
+        }
+      )
+    },
+    created() {
+      this.id = window.location.href;
+      this.macAddress = config[this.id.substring(this.id.indexOf('=') + 1, this.id.indexOf('#'))];
+    },
+    methods: {
+
+      goBack() {
+        router.push("Registration")
+      },
+
+      itemClick(item) {
+        router.push('Reservation');
+        this.$store.dispatch('reservation/data', item);
+      },
+
+      takePhoto() {
+        this.$http.get('/photo').then(response => {
+          console.log(response);
+          // this.imagePath+=response;
+        })
+      },
+
+      notify() {
+
+        console.log(this.items);
+        console.log(this.imagePath);
+
+        let bodyNotification = {
+          "notification": {
+            "title": "У вас новый пациент",
+            "body": "К вам пришел(ла): " + this.items[0].fullname
           },
-          table:[],
-          show: false,
-          label: "Пожалуйста, подождите...",
-          progress: false,
-          msg: "ДЕТАЛИ ПРИЕМА",
-          check: false,
-          macAddress: ''
-        }
-      },
-      beforeRouteEnter: (to, from, next) => {
-          next(vm => { vm.items = vm.$store.getters['registration/getData']; }
-        )
-      },
-      created() {
-        this.id = window.location.href;
-        this.macAddress = config[this.id.substring(this.id.indexOf('=')+1,this.id.indexOf('#'))];
-      },
-      methods: {
+          "data": {
+            "name": this.items[0].fullname,
+            "time": this.items[0].bhour+':'+this.items[0].bmin,
+            "photo": this.imagePath
+          },
+          "to": "/topics/" + "55"
+        };
 
-        goBack() {
-          router.push("Registration")
-        },
-
-        itemClick(item) {
-          router.push('Reservation');
-          this.$store.dispatch('reservation/data', item);
-        },
-
-        takePhoto() {
-          this.$http.get('/photo').then(response => {
-            console.log(response);
-            this.imagePath = location.origin+"/images/";
+        this.$http.post('https://fcm.googleapis.com/fcm/send', bodyNotification, {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": 'key=AAAAdArWtQU:APA91bEBGdgYLIUuX0_9H7MITtswX8Eu4YYMfNDUoVMfInHz0ueCtIL1JBtPRRbzievC3JhLApscOsx7zhpSNkxkJ5He8QjnXJFB5MQ6tQuhjv2zW6jUqhmBLuT7QYs0brG_73vJt5iT'
+          }
+        })
+          .then(response => {
+            console.log("Notification: ", response)
           })
-        },
+      },
 
-        notify() {
+      checkIncome() {
+        this.progress = true;
+        this.$http.get('/submit/{dcode}/{pcode}/{bhour}/{bmin}/{fhour}/{fmin}/{schedid}/{cashid}/{chid}/{date}/{filial}', {
+          params: {
+            dcode: this.items[0].dcode,
+            pcode: this.items[0].pcode,
+            bhour: this.items[0].bhour,
+            bmin: this.items[0].bmin,
+            fhour: this.items[0].fhour,
+            fmin: this.items[0].fmin,
+            schedid: this.items[0].schedid,
+            cashid: this.items[0].cashid,
+            chid: this.items[0].chid,
+            date: moment().format('DD.MM.YYYY'),
+            filial: this.macAddress
+          }
+        })
+          .then(response => {
+            this.progress = false;
+            this.check = true;
+            this.disabled = true;
+          });
 
-          console.log(this.imagePath);
-
-          let bodyNotification = { "notification": {"title": "К вам пациент!", "body": "К вам пришел(ла): "+this.table.fullname},
-            "to":"/topics/"+"55"
-          };
-
-          this.$http.post('https://fcm.googleapis.com/fcm/send', bodyNotification, {headers: {"Content-Type":"application/json", "Authorization": 'key=AAAAdArWtQU:APA91bEBGdgYLIUuX0_9H7MITtswX8Eu4YYMfNDUoVMfInHz0ueCtIL1JBtPRRbzievC3JhLApscOsx7zhpSNkxkJ5He8QjnXJFB5MQ6tQuhjv2zW6jUqhmBLuT7QYs0brG_73vJt5iT'}})
-            .then(response => {
-              console.log("Notification: ",response)
-            })
-        },
-
-        checkIncome() {
-          this.progress = true;
-          this.$http.get('/submit/{dcode}/{pcode}/{bhour}/{bmin}/{fhour}/{fmin}/{schedid}/{cashid}/{chid}/{date}/{filial}', {params: {dcode:this.items[0].dcode, pcode:this.items[0].pcode, bhour:this.items[0].bhour, bmin:this.items[0].bmin,
-            fhour:this.items[0].fhour, fmin:this.items[0].fmin, schedid:this.items[0].schedid, cashid:this.items[0].cashid, chid: this.items[0].chid, date: moment().format('DD.MM.YYYY'), filial: this.macAddress}})
-            .then(response => {
-              this.progress = false;
-              this.check = true;
-              this.disabled=true;
-            });
-
-          this.takePhoto();
-          this.notify();
-        }
+        this.takePhoto();
+        this.notify();
       }
     }
+  }
 </script>
 
 <style scoped>
@@ -132,7 +169,7 @@
     color: white;
     background-color: #ff6a13;
     background-image: linear-gradient(340deg, #ff6a13, rgba(255, 237, 226, 0.34));
-    box-shadow: 0px 5px 0px #b44813, 0px 3px 15px rgba(0,0,0,.4);
+    box-shadow: 0px 5px 0px #b44813, 0px 3px 15px rgba(0, 0, 0, .4);
   }
 
   .main_details {
@@ -143,10 +180,11 @@
     display: flex;
     height: 100%;
   }
-  .details_iframe{
+
+  .details_iframe {
     width: 51%;
     position: relative;
-    top:-225px;
+    top: -225px;
   }
 
   .logo {
--- src/main/resources/static/src/pages/reservation.vue
+++ src/main/resources/static/src/pages/reservation.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="reservation">
     <b_img class="logo" src="../photo/55/start.png"></b_img>
-    <p style="position: relative; right: 335px; color: #009800; font-size: 30px" class="mt-3 mb-4"><strong>{{msg}}</strong></p>
+    <p style="position: relative; right: 335px; color: #009800; font-size: 30px" class="mt-3 mb-4">
+      <strong>{{msg}}</strong></p>
     <div class="reservation_details">
-        <div style="background: #ffd310; width: 49%">
+      <div style="background: #ffd310; width: 49%">
 
-          <p><strong>{{table.fullname}},</strong></p>
-          <p>ВАШ ПРИЕМ СОСТОИТСЯ</p>
-          <p>{{table.workdate}} В {{table.bhour}}:{{table.bmin}}</p>
-          <p>ДОКТОР {{table.docname}}</p>
-          <p>ПРИГЛАСИТ ВАС В КАБИНЕТ {{table.chname}}</p>
+        <p><strong>{{table.fullname}},</strong></p>
+        <p>ВАШ ПРИЕМ СОСТОИТСЯ</p>
+        <p>{{table.workdate.substring(0,11)}} В {{table.bhour}}:{{table.bmin}}</p>
+        <p>ДОКТОР {{table.docname}}</p>
+        <p>ПРИГЛАСИТ ВАС В КАБИНЕТ {{table.chname}}</p>
 
-        <b_img class="mb-3" height="375" rounded src="../photo/55/chname/doktor.png"/> <br/>
+        <b_img class="mb-3" height="375" rounded src="../photo/55/chname/doktor.png"/>
+        <br/>
 
         <button class="btn btn-lg mt-3" @click="goBack">Назад</button>
-        <button class="btn btn-warning btn-lg mt-3" @click="takeImage">Фото</button>
+        <!--<button class="btn btn-warning btn-lg mt-3" @click="takeImage">Фото</button>-->
         <!--<button class="btn btn-warning btn-lg mt-3" @click="notify">Уведомление</button>-->
 
-        </div>
+      </div>
 
-      <div class="reservation_iframe"><b_img src="../photo/55/sales/55.png" style="width: 100%; height: 100%"/></div>
+      <div class="reservation_iframe">
+        <b_img src="../photo/55/sales/55.png" style="width: 100%; height: 100%"/>
+      </div>
 
     </div>
 
@@ -27,54 +31,71 @@
 </template>
 
 <script>
-    import list_group from "bootstrap-vue/es/components/list-group/list-group";
-    import list_group_item from "bootstrap-vue/es/components/list-group/list-group-item";
-    import Button from "bootstrap-vue/es/components/button/button";
-    import Card from "bootstrap-vue/es/components/card/card";
-    import moment from 'moment';
-    import router from '../router/index';
-    import b_img from "bootstrap-vue/es/components/image/img";
+  import list_group from "bootstrap-vue/es/components/list-group/list-group";
+  import list_group_item from "bootstrap-vue/es/components/list-group/list-group-item";
+  import Button from "bootstrap-vue/es/components/button/button";
+  import Card from "bootstrap-vue/es/components/card/card";
+  import router from '../router/index';
+  import b_img from "bootstrap-vue/es/components/image/img";
 
-    export default {
-      name: "Reservation",
-      components: {list_group, list_group_item, Button, Card, b_img},
+  export default {
+    name: "Reservation",
+    components: {list_group, list_group_item, Button, Card, b_img},
 
-      data() {
-        return {
-          table: [],
-          msg: "ДЕТАЛИ ПРИЕМА"
+    data() {
+      return {
+        table: [],
+        msg: "ДЕТАЛИ ПРИЕМА"
+      }
+    },
+
+    beforeRouteEnter: (to, from, next) => {
+      next(vm => {
+          vm.table = vm.$store.getters['reservation/getData'];
+          console.log(vm.table);
         }
+      )
+    },
+
+    methods: {
+      goBack() {
+        router.push("Details")
+      },
+      takeImage() {
+        this.$http.get('/photo').then(response => {
+          console.log(response);
+
+          this.imagePath = location.origin+"/images/";
+        })
       },
 
-      beforeRouteEnter: (to, from, next) => {
-        next(vm => {
-            vm.table = vm.$store.getters['reservation/getData'];
-            console.log(vm.table);
+      //TODO: Remove it later
+      notify() {
+        let bodyNotification = {
+          "notification": {
+            "title": "У вас новый пациент",
+            "body": "К вам пришел(ла): " + this.table.fullname
+          },
+          "data": {
+            "name": this.table.fullname,
+            "time": this.table.bhour + ":" + this.table.bmin,
+            "photo": ""
+          },
+          "to": "/topics/"+"55"
+        };
+
+        this.$http.post('https://fcm.googleapis.com/fcm/send', bodyNotification, {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": 'key=AAAAdArWtQU:APA91bEBGdgYLIUuX0_9H7MITtswX8Eu4YYMfNDUoVMfInHz0ueCtIL1JBtPRRbzievC3JhLApscOsx7zhpSNkxkJ5He8QjnXJFB5MQ6tQuhjv2zW6jUqhmBLuT7QYs0brG_73vJt5iT'
           }
-        )
-      },
-
-      methods: {
-        goBack() {
-          router.push("Details")
-        },
-        takeImage() {
-          this.$http.get('/photo').then(response => {
-            console.log(response);
+        })
+          .then(response => {
+            console.log("Notification: ", response)
           })
-        },
-        notify() {
-          let bodyNotification = { "notification": {"title": "К вам пациент!", "body": "К вам пришел: "+this.table.fullname},
-            "to":":"
-          };
-
-          this.$http.post('https://fcm.googleapis.com/fcm/send', bodyNotification, {headers: {"Authorization": 'key=AAAAdArWtQU:APA91bEBGdgYLIUuX0_9H7MITtswX8Eu4YYMfNDUoVMfInHz0ueCtIL1JBtPRRbzievC3JhLApscOsx7zhpSNkxkJ5He8QjnXJFB5MQ6tQuhjv2zW6jUqhmBLuT7QYs0brG_73vJt5iT'}})
-            .then(response => {
-            console.log("Notification: ",response)
-          })
-        }
       }
     }
+  }
 </script>
 
 <style scoped>
@@ -88,7 +109,7 @@
     color: white;
     background-color: #ff6a13;
     background-image: linear-gradient(340deg, #ff6a13, rgba(255, 237, 226, 0.34));
-    box-shadow: 0px 5px 0px #b44813, 0px 3px 15px rgba(0,0,0,.4);
+    box-shadow: 0px 5px 0px #b44813, 0px 3px 15px rgba(0, 0, 0, .4);
   }
 
   .reservation {
@@ -96,15 +117,17 @@
   }
 
   .reservation_details {
-    display:flex;
-    height:100%;
+    display: flex;
+    height: 100%;
   }
-  .reservation_iframe{
+
+  .reservation_iframe {
     width: 51%;
-    height:100%;
+    height: 100%;
     position: relative;
-    top:-225px;
+    top: -225px;
   }
+
   .logo {
     position: relative;
     right: 40%;
